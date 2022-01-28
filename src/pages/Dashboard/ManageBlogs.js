b1b64ba648094194d57e33fe81b1f15ca7de697b
monirul-fahad <immonirulfahad@gmail.com>
2022-01-28 20:15:15 +0600

    manage blogs

--- src/pages/Dashboard/ManageBlogs.js
+++ src/pages/Dashboard/ManageBlogs.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import AdminSidebar from "../AdminSidebar";
+import { RiDeleteBin7Line } from "react-icons/ri";
+import { GrView } from "react-icons/gr";
+import { FiEdit } from "react-icons/fi";
 
 const ManageBlogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -11,16 +14,31 @@ const ManageBlogs = () => {
   }, []);
   return (
     <div className="dashboard-content">
-      <div className="md:col-span-3 hidden md:block">
+      <div className="md:col-span-2 hidden md:block">
         <AdminSidebar />
       </div>
-      <div className="md:col-span-9 col-span-12">
+      <div className="md:col-span-10 col-span-12 pr-3">
         <div className="grid grid-cols-12 gap-4">
           {blogs.map((blog) => (
-            <div className="md:col-span-3 col-span-6">
-              <img src={blog.img} alt="" className="w-full h-60 object-cover	" />
-              <h6>{blog.title}</h6>
+            <div className="md:col-span-3 col-span-6 p-3 shadow-lg">
+              <img
+                src={blog.img}
+                alt=""
+                className="w-full h-60 object-cover rounded	"
+              />
+              <h4 className="mt-3">{blog.title}</h4>
               <p>Posted by {blog.name}</p>
+              <ul className="flex gap-4 justify-center mt-4">
+                <li>
+                  <GrView className="text-4xl bg-java hover:bg-java-600 p-2 rounded cursor-pointer" />
+                </li>
+                <li>
+                  <FiEdit className="text-4xl bg-java hover:bg-java-600 p-2 rounded cursor-pointer" />
+                </li>
+                <li>
+                  <RiDeleteBin7Line className="text-4xl bg-java hover:bg-red-600 p-2 rounded cursor-pointer" />
+                </li>
+              </ul>
             </div>
           ))}
         </div>
